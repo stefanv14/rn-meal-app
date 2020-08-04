23f7a655df4a7c4a3a0363a08c84e8a3584c846a
stefanv14 <vasilijevic032@gmail.com>
2020-08-04 11:50:32 +0200

    refactor(fetch-data): fetch data from firebase
    fetch data from firebase

--- src/screens/CategoryMealsScreen/CategoryMealsScreen.jsx
+++ src/screens/CategoryMealsScreen/CategoryMealsScreen.jsx
@@ -1,11 +1,49 @@
 import { PropTypes } from 'prop-types'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ActivityIndicator, View } from 'react-native'
 import MealsList from '../../../components/MealsList'
 import CategoriesContext from '../../../context/CategoriesContext'
+import Meal from '../../../models/meal'
+import { styles } from './CategoryMealsScreen.styles'
 
 const CategoryMealScreen = ({ navigation, route }) => {
-  const value = useContext(CategoriesContext) // eslint-disable-line
+  const [isLoading, setLoading] = useState(true)
+
+  const value = useContext(CategoriesContext)
   const catId = route.params.categoryId
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    fetch('https://rn-meals-app-a099c.firebaseio.com/meals.json')
+      .then((response) => response.json())
+      .then((json) => {
+        const loadedMeals = []
+
+        for (const key in json) {
+          loadedMeals.push(
+            new Meal(
+              json[key].id,
+              json[key].categoryIds,
+              json[key].title,
+              json[key].affordability,
+              json[key].complexity,
+              json[key].imageUrl,
+              json[key].duration,
+              json[key].ingredients,
+              json[key].steps,
+              json[key].isGlutenFree,
+              json[key].isVegan,
+              json[key].isVegetarian,
+              json[key].isLactoseFree,
+            ),
+          )
+        }
+
+        value.setMeals(loadedMeals)
+      })
+      .catch((error) => console.error(error)) //eslint-disable-line
+      .finally(() => setLoading(false))
+  }, [])
 
   const availableMeals = [...value.meals]
 
@@ -13,7 +51,13 @@ const CategoryMealScreen = ({ navigation, route }) => {
     (meal) => meal.categoryIds.indexOf(catId) >= 0,
   )
 
-  return <MealsList listData={displayedMeals} navigation={navigation} />
+  return isLoading ? (
+    <View style={styles.spinner}>
+      <ActivityIndicator color="#999999" size="large" />
+    </View>
+  ) : (
+    <MealsList listData={displayedMeals} navigation={navigation} />
+  )
 }
 
 CategoryMealScreen.propTypes = {
